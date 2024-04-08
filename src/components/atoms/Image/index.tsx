212081b754f7c95b3ac14388d2f8c242540cc688
Gustavo Pontes <gustavopontes@mbp-de-gustavo.lan>
2024-04-08 00:07:06 -0300

    feat: quality Image

--- src/components/atoms/Image/index.tsx
+++ src/components/atoms/Image/index.tsx
@@ -3,7 +3,7 @@ import { ImageProps } from "./props";
 import * as S from "./styles";
 
 const Image: FC<ImageProps> = ({ alt, ...props }) => {
-  return <S.Img {...props} alt={alt} loading="eager" />;
+  return <S.Img {...props} alt={alt} priority={true} quality={100} />;
 };
 
 export default Image;
